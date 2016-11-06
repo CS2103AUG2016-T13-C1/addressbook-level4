73510ca81346beec79ff60a4710da83863e6bad7
VivianLow <vivian198912@gmail.com>
2016-11-07 03:44:04 +0800

    Remove test from StoreCommandTest

--- src/test/java/guitests/StoreCommandTest.java
+++ src/test/java/guitests/StoreCommandTest.java
@@ -7,25 +7,12 @@ import seedu.task.logic.commands.StoreCommand;
 
 import static seedu.task.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.io.File;
-
 public class StoreCommandTest extends TaskBookGuiTest{
 	
 	@Test
 	public void store_invalidPath() {
 		commandBox.runCommand("store + 123");
 		assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, StoreCommand.MESSAGE_USAGE));
-	}
-	
-	@Test
-	public void store_validPath() {
-	    String newSaveLocation = "src\\test\\data\\sandbox";
-	    commandBox.runCommand("store " + newSaveLocation);
-	    newSaveLocation += "\\taskbook.xml";
-	    assertResultMessage(String.format(StoreCommand.MESSAGE_SUCCESS, newSaveLocation));
-	    
-	    File userConfig = new File("userConfig.json");
-	    userConfig.delete();
 	}
 	
 }
